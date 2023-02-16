0a356766864cc826f026233d0bc9e06098344b38
zzc-1024 <1977309465@qq.com>
2023-02-16 15:53:43 +0800

    修改工具类的逻辑错误

--- include/Wonderland/Utilities/SetOrigin.hpp
+++ include/Wonderland/Utilities/SetOrigin.hpp
@@ -1,15 +1,17 @@
 #pragma once
 
+#include <cassert>
+
 #include <SFML/Graphics.hpp>
 
 namespace Utilities {
 
 template <typename T>
 void setCenter(T &t) {
-    static_assert(std::is_same<sf::Transformable, T>, 
-        "The type must be the derived class of sf::Transformable, not sf::Transformable itself.");
-    static_assert(!std::is_base_of<sf::Transformable, T>,
-        "The type must be the derived class of sf::Transformable.")
+    //static_assert(std::is_same<sf::Transformable, T>::value, 
+    //    "The type must be the derived class of sf::Transformable, not sf::Transformable itself.");
+    static_assert(std::is_base_of<sf::Transformable, T>::value,
+        "The type must be the derived class of sf::Transformable.");
     sf::FloatRect lb = t.getLocalBounds();
     t.setOrigin(lb.left + lb.width / 2, lb.top + lb.height / 2);
 }
@@ -19,10 +21,10 @@ void (*setCentre)(T &t) = setCenter;
 
 template <typename T>
 void setLeftTop(T &t) {
-    static_assert(std::is_same<sf::Transformable, T>, 
-        "The type must be the derived class of sf::Transformable, not sf::Transformable itself.");
-    static_assert(!std::is_base_of<sf::Transformable, T>,
-        "The type must be the derived class of sf::Transformable.")
+    //static_assert(std::is_same<sf::Transformable, T>::value, 
+    //    "The type must be the derived class of sf::Transformable, not sf::Transformable itself.");
+    static_assert(std::is_base_of<sf::Transformable, T>::value,
+        "The type must be the derived class of sf::Transformable.");
     sf::FloatRect lb = t.getLocalBounds();
     t.setOrigin(lb.left, lb.top);
 }
